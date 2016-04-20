a28b2295976e84eb110e8009dcf79b88989705c5
rospetbaty <petros@sourced.tech>
2016-04-20 22:49:01 +0200

    First commit

---
+++ src/app.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import TodoBox from './index.jsx';
+
+let data = [
+  {
+    title: "Test"
+  }
+];
+
+document.addEventListener('DOMContentLoaded', function() {
+  ReactDOM.render(
+    <h1>"hello"</h1>,
+    document.getElementById('container')
+  );
+}, false);
